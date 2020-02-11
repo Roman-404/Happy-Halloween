b3eb42fc4426686f0c265312524d580f2d13264b
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-02-11 17:19:25 +0300

    ...

--- happy-halloween/src/Timer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import moment from 'moment';
-
-const Timer = ({date}) => {
-const diff = moment(date).diff(moment().format())
-    return moment(diff).format('DDD:hh:mm:ss')
-}
-
-export default Timer;
--- happy-halloween/src/page-info.jsx
+++ happy-halloween/src/page-info.jsx
@@ -2,19 +2,22 @@ import React, {Component} from 'react';
 import Tasks from './tasks';
 import react_loading from './graphics/react-loading.png';
 import axios from 'axios';
-import Timer from './Timer';
+import moment from 'moment';
 
 export default class PageInfo extends Component {
     state = {
         loading: true,
         tasks: [],
-        date: null
+        date_to: null,
+        timer: null,
+        enum_dates: ['дней', 'часов', 'минут', 'секунд']
     }
 
     componentDidMount() {
         this.loadTasks()
         setInterval(() => {
             this.setState({
+                timer: this.getTimer(this.state.date_to)
             })
         }, 1000);
     }
@@ -27,7 +30,7 @@ export default class PageInfo extends Component {
         .then(data => {
             this.setState({
                 tasks: data.tasks,
-                date: data.endsAt,
+                date_to: data.endsAt,
                 loading: false
             })
         })
@@ -36,14 +39,22 @@ export default class PageInfo extends Component {
         })
     }
 
+    getTimer = date => {
+        const diff = moment(date).diff(moment().format())
+        const timer = moment(diff).format('DDD:hh:mm:ss').split(':')
+    return timer
+    }
+
 
     render() {
-        const {loading, tasks, date} = this.state;
+        const {loading, tasks, timer, enum_dates} = this.state;
         return(
             <div>
                 <img className='page-info' src={require('./graphics/header.png')} alt=''/>
                   <div className='background'>
-                      <h1 className='timer'><Timer date={date}/></h1>
+                      <h1 className='timer'>{timer ? timer.map((e,k) => <span key={k} className='font-title'>
+                          {e + (timer.length-1 === k ? '' : ':')}
+                            <div className='font-subtitle'>{enum_dates.find((i,key) => k===key)}</div></span>) : null}</h1>
                       {loading ? <img src={react_loading} className='react-loading' alt='' />
                       :
                      <Tasks tasks={tasks}
