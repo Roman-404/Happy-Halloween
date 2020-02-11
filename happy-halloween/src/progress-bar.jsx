c8b7425c5962087a1b0adf0c9972de04917d8261
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-02-11 22:21:01 +0300

    ...

--- happy-halloween/src/progress-bar.jsx
+++ happy-halloween/src/progress-bar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Progress = ({done, handleSetShirt}) => {
 	const [style, setStyle] = React.useState({});
+	const [progress=false, setProgress] = React.useState({});
 	
 	setTimeout(() => {
 		const newStyle = {
@@ -10,12 +11,21 @@ const Progress = ({done, handleSetShirt}) => {
 		}
 		
 		setStyle(newStyle);
-	}, 200);
+	}, 1000);
+
+	setTimeout(() => {
+		if (done === 100) {
+		const newProgress = {
+			done: true
+		}
+		
+		setProgress(newProgress)};
+	}, 2000);
 	
 	return (
 		<div className="progress">
 			<div className="progress-done" style={style}/>
-			   {done === 100 ? 
+			   {progress.done ? 
                   <button className='btn' onClick={handleSetShirt}>Поставить рубашку</button> : null
                }
 		</div>
